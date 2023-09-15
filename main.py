2ec74eac1c41687224bd3d4af5eedf6aef4547d1
Nathan <nathan@nzm.ca>
2023-09-15 17:19:48 -0400

    properly listing files in folders, almost

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import argparse
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from pathlib import Path
+import queue
 
-SCOPES = ['https://googleapis.com/auth/drive']
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-C', '--credentials', required=True, help='credentials.json')
@@ -13,29 +16,55 @@ parser.add_argument('-t', '--to_folder', help='To team drive')
 
 
 def authenticate(filename_token, filename_credentials):
-    creds = Credentials.from_authorized_user_file(filename_token, SCOPES)
+    creds = None
+    if Path(filename_token).exists():
+        creds = Credentials.from_authorized_user_file(filename_token, SCOPES)
     if not creds or not creds.valid:
         if creds and not creds.expired and creds.refresh_token:
             creds.Refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets(filename_credentials, SCOPES)
-            creds = flow.run_local_server(port=0)
+            flow = InstalledAppFlow.from_client_secrets_file(filename_credentials, SCOPES)
+            creds = flow.run_local_server(port=0, open_browser=False)
             with open(filename_token, 'w') as token:
                 token.write(creds.to_json())
     return creds
 
 args = parser.parse_args()
 
-def list_files_in_folder(folder_id, service):
-    response = service.files().list(f'" {top_level_folder} " in parents').execute()
-    return response
+def list_contents_of_folder(folder_id, service):
+    return service.files().list(q=f'"{folder_id}" in parents').execute()
+
+
+def recurse_folders(folder_id_top, service):
+    q = queue.Queue()
+
+    q.put((folder_id_top, ''))
+    while True:
+        if q.empty():
+            print('done')
+            return
+        else:
+            (folder_id, name) = q.get()
+            response = list_contents_of_folder(folder_id, service)
+            if getattr(response, 'nextPageToken', None):
+                next_page_token = response['nextPageToken']
+            for r in response['files']:
+                if r['mimeType'] == 'application/vnd.google-apps.folder':
+                    q.put((r['id'], r['name']))
+                    print(r['name'])
+        
+    
+    
 
 from_id = args.from_folder
 to_id = args.to_folder
 
-authenticate(args.credentials, args.token)
+creds = authenticate(args.token, args.credentials)
 
 with build('drive', 'v3', credentials=creds) as service:
     # list directory for now
-    print(list_files_in_folder(from_id, service))
+    #r = list_contents_of_folder(from_id, service)
+    recurse_folders(from_id, service)
+
+
 
